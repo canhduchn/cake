5fa9c47e9566ed19294ddc0d8a11f14ba6a2f861
canhduchn <83177122+canhduchn@users.noreply.github.com>
2022-12-08 00:35:55 +0700

    update

---
+++ app/script/slide.js
@@ -0,0 +1,27 @@
+const slides=[
+    {
+        number: "01/04",
+        title: "Ice Cream Shop",
+        text: "consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore magna aliqua",
+        img:"./app/images/slider-img.png",
+    },
+    {
+        number: "02/04",
+        title: "Ice Cream Shop",
+        text: "consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore magna aliqua",
+        img:"./app/images/slider-img.png",
+    },
+    {
+        number: "03/04",
+        title: "Ice Cream Shop",
+        text: "consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore magna aliqua",
+        img:"./app/images/slider-img.png",
+    },
+    {
+        number: "04/04",
+        title: "Ice Cream Shop",
+        text: "consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore magna aliqua",
+        img:"./app/images/slider-img.png",
+    }
+]
+    
